c96561558f4dab679c4c6c7420ac509441b61339
Алексей <askerbl67@gmail.com>
2020-01-12 14:25:25 +0300

    ПП, критерии, + вопросы в телеграмме

--- gulpfile.js
+++ gulpfile.js
@@ -106,22 +106,6 @@ gulp.task("copy", function () {
     .pipe(gulp.dest("build"));
 });
 
-/*gulp.task("server", function () {
-  server.init({
-    server: "source/",
-    notify: false,
-    open: true,
-    cors: true,
-    ui: false
-  });
-
-   gulp.watch("source/", gulp.series("css"));
-   gulp.watch("source/*.html").on("change", server.reload);
-
- });*/
-
-
-
 gulp.task("server", function () {
   server.init({
     server: "build/",
